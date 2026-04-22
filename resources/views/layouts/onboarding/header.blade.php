ea21e00fa65251e55e850d8479d0a7be9b01fca4
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2026-04-22 09:56:14 +0700

    update controller & done

--- resources/views/layouts/onboarding/header.blade.php
+++ resources/views/layouts/onboarding/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hidroponik GM 200}</title>
+    <title>Hidroponik GM 200</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
